e96a0180f4bdfde5606931fcf1a850b36b488001
VIPon4ik <umniyd@gmail.com>
2024-01-04 00:05:41 +0200

    Done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,50 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.floor(Math.random() * 10);
+  }
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) { }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[];
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super()
+    this.key = key;
+  }
+
+  openDoor(key: Key): void {
+    if (this.key === key) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -8,4 +55,4 @@ house.openDoor(person.getKey());
 house.comeIn(person);
 
 
-export {};
+export { };
